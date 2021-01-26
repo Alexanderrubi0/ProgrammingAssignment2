c373419064329595ed3729f460d62058c1a1f9ee
Nestor Rubio <alexrv.12@hotmail.com>
2021-01-25 20:48:32 -0500

    Tarea ajustadad

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,42 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  Num_inverse <- null
 
+  #Asignar valores a la matriz
+  
+  set <- function(Mtrz){
+    
+    mz <<- Mtrz
+    Num_inverse <<- NULL
+    
+  }
+    get <- funcion()mz
+    setInverse <-function(inverse) in <<- inverse
+    
+    getInverse <-function()Num_inverse
+    
+    list(set = set, 
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+
+  
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mz, ...) {
         ## Return a matrix that is the inverse of 'x'
+  Num_inverse <- mz$getInverse()
+  if(!is.null(Num_inverse)){
+    message("Getting cached data inverse")
+    return(Num_inverse)
+  }
+    mat <-mz$get()
+    Num_inverse <- solve(mat, ...)
+    mz$set.inverse(Num_inverse)
+    Num_inverse
 }
